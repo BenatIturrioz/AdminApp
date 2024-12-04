62a86ac13f192a34f9f98d0b5c3b26a4ec49c83a
BenatIturrioz <beitumu7@gmail.com>
2024-12-04 16:14:52 +0100

    Dominio karoeta sortuta eta erabiltzaile klasea sortuta barruan

--- AdminApp/AdminApp/Connection.cs
+++ AdminApp/AdminApp/Connection.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,35 @@ namespace AdminApp
 {
     internal class Connection
     {
-        private 
+        private readonly string connectionString = "server='localhost';port='3306';user id='root'; password = '1WMG2023'; database = 'erronka1';SslMode = 'none'";
+
+        public MySqlConnection GetConnection()
+        {
+            try
+            {
+                MySqlConnection connection = new MySqlConnection(connectionString);
+                return connection;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al crear la conexión: " + ex.Message);
+            }
+        }
+
+        public bool TestConnection()
+        {
+            try
+            {
+                using (MySqlConnection connection = GetConnection())
+                {
+                    connection.Open();
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
