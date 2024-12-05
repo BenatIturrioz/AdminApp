dc87bf7dcb5e65b6b2108da3f75be883c29141fd
BenatIturrioz <beitumu7@gmail.com>
2024-12-05 14:11:32 +0100

    App.config eta mapeoa egiteko xml artxibo sortuta eta produktu klasea sortuta

---
+++ AdminApp/AdminApp/Dominio/Produktua.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminApp.Dominio
+{
+    internal class Produktua
+    {
+        public virtual int Id { get; set; }
+        public virtual string Izena { get; set; }
+        public virtual string Deskribapena { get; set; }
+        public virtual int Kantitatea { get; set; }
+        public virtual int KantitateMin { get; set; }
+        public virtual float Prezioa { get; set; }
+        public virtual int Mota { get; set; }
+        public virtual float ErosketaPrezioa { get; set; }
+
+
+    }
+}
