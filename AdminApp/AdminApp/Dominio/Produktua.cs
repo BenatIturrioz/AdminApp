1d0a965e539b70794c44f04c2b785d35a00c8b83
HaimarAr <hai_ari_mor@goierrieskola.org>
2025-01-08 16:44:47 +0100

    Erakutsi profuktuak eginda

--- AdminApp/AdminApp/Dominio/Produktua.cs
+++ AdminApp/AdminApp/Dominio/Produktua.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace AdminApp.Dominio
 {
@@ -19,6 +22,28 @@ namespace AdminApp.Dominio
         public virtual int ErosketaKantitatea { get; set; }
         public virtual byte Aktibo { get; set; }
 
+        public static void ProduktuakErakutsi(DataGridView dataGridView1)
+        {
+            // Crear una instancia de la conexión
+            Connection connection = new Connection();
+            string query = "SELECT * FROM produktua";
+
+            using (MySqlConnection konexioa = connection.GetConnection())
+            {
+                konexioa.Open();
+                using (MySqlCommand cmd = new MySqlCommand(query, konexioa))
+                {
+                    MySqlDataAdapter dataAdapter = new MySqlDataAdapter(cmd);
+                    DataTable tabla1 = new DataTable();
+                    dataAdapter.Fill(tabla1);
+                    dataGridView1.DataSource = tabla1;
+                }
+
+                // Ajustar las columnas y filas del DataGridView
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+                dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            }
+        }
 
     }
 }
---
+++ AdminApp/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        private void produktuakErakutsi()
+        {
+            Connection connection = new Connection();
+            string query = "SELECT * FROM produktua";
+
+            using (MySqlConnection konexioa = connection.GetConnection())
+            {
+                konexioa.Open();
+                using (MySqlCommand cmd = new MySqlCommand(query, konexioa))
+                {
+                    MySqlDataAdapter dataAdapter = new MySqlDataAdapter(cmd);
+                    DataTable tabla1 = new DataTable();
+                    dataAdapter.Fill(tabla1);
+                    dataGridView1.DataSource = tabla1;
+                }
+
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+                dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+
+            }
+        }
+    }
+}
